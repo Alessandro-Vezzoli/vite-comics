d8f18025da97f5088125d210549e6db6a8f77fb6
VEZZO\alexv <alessandro.vezzoli@vp360.eu>
2024-01-31 22:59:43 +0100

    start footer (not finish)

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@
     <AppHeader></AppHeader>
     <AppContent></AppContent>
     <AppPreFooter></AppPreFooter>
+    <div class="background-img">
+      <div class="main">
+        <div class="container">
+          <h1>ciao</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -18,4 +25,17 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.container {
+  display: flex;
+  align-items: left;
+  justify-content: center;
+}
+.background-img {
+  background-image: url("./img/footer-bg.jpg");
+  background-repeat: no-repeat;
+  background-position: top;
+  background-size: cover;
+  height: 35vh;
+}
+</style>
